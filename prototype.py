15fa330817bfcc4d2d7a0c2ae6ba10e94329a140
Martyn Garcia <martyn.garcia@gmail.com>
2023-11-11 17:04:33 -0700

    initial create flow

--- prototype.py
+++ prototype.py
@@ -18,5 +18,13 @@ def login():
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route('/action-set/new')
+def action_set_create():
+    return render_template('action_set_create.html')
+
+@app.route('/action-set/<id>')
+def action_set_show(id):
+    return render_template('action_set_show.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10002)
