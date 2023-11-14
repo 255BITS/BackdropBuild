0ad353f1e41d94de2d3275fecf96fba47d0285c1
Mikkel Garcia <mikkel@255bits.com>
2023-11-14 15:46:15 -0700

    extract service / api edit

--- portal/app/routes/api_routes.py
+++ portal/app/routes/api_routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, jsonify, url_for, flash
 from shared.couch import db
-import re
+from app.services.api_service import parse_api_object
 
 from flask import Blueprint
 api_bp = Blueprint('apis', __name__)
@@ -16,7 +16,7 @@ def apis_my():
 
 @api_bp.route('/apis/new')
 def apis_new():
-    return render_template('api_new.html')
+    return render_template('api_new.html', errors={}, api={})
 
 @api_bp.route('/apis/create', methods=["POST"])
 def apis_create():
@@ -24,55 +24,10 @@ def apis_create():
     if request.content_type != 'application/x-www-form-urlencoded':
         return jsonify({"error": "Content type must be application/x-www-form-urlencoded"}), 400
 
-    errors = {}
-
-    # Extract the fixed attributes
-    name = request.form.get("name", "")
-    defaultFunctionName = request.form.get("defaultFunctionName", "")
-    method = request.form.get("method", "")
-    url = request.form.get("url", "")
-    shortDescription = request.form.get("shortDescription", "")
-
-    # Validation checks
-    if not name:
-        errors['name'] = 'Name cannot be empty.'
-    # TODO: Check for name uniqueness in the database or data store
-    if not shortDescription:
-        errors['shortDescription'] = 'Short description cannot be empty.'
-    if not url:
-        errors['url'] = 'URL cannot be empty.'
-    elif not re.match(r'^https://', url):
-        errors['url'] = 'URL must start with https://'
-
-    # Initialize an empty list to store the parameters
-    params = []
-
-    # Iteratively process each pair of 'type' and 'input'
-    i = 0
-    while True:
-        param_type_key = f'param{i}-type'
-        param_input_key = f'param{i}-input'
-
-        # Check if both keys exist in the form data
-        if param_type_key in request.form and param_input_key in request.form:
-            params.append([request.form[param_type_key], request.form[param_input_key]])
-            i += 1
-        else:
-            break
-
+    errors, api_object = parse_api_object(request)
     # If there are errors, render the api_new template with errors
     if errors:
-        return render_template('api_new.html', errors=errors, name=name, defaultFunctionName=defaultFunctionName, method=method, url=url, shortDescription=shortDescription, params=params)
-
-    api_object = {
-        "name": name,
-        "defaultFunctionName": defaultFunctionName,
-        "method": method,
-        "url": url,
-        "shortDescription": shortDescription,
-        "type": "API",
-        "params": params
-    }
+        return render_template('api_new.html', errors=errors, api={"name":name, "defaultFunctionName":defaultFunctionName, "method":method, "url":url, "shortDescription":shortDescription, "params":params})
     new_api_object = db.save(api_object)
 
     # Add your logic here to handle the attributes and 'params' list
@@ -80,12 +35,27 @@ def apis_create():
 
 @api_bp.route('/apis/<id>', methods=["GET"])
 def apis_show(id):
+    # TODO security
     api = db.get(id)
-    print(api)
     return render_template('api_show.html', api=api)
+
+@api_bp.route('/apis/<id>/edit', methods=["GET"])
+def apis_edit(id):
+    # TODO security
+    api = db.get(id)
+    return render_template('api_new.html', errors=[], api=api)
+
+@api_bp.route('/apis/<id>/update', methods=["POST"])
+def apis_update(id):
+    # TODO security
+    errors, api_object = parse_api_object(request)
+    api_object["id"]=id
+    db.save(api_object)
+    return render_template('api_show.html', errors=[], api=api_object)
 
 @api_bp.route('/apis/<id>/publish', methods=["POST"])
 def apis_publish(id):
+    # TODO security
     api = db.get(id)
     api["visibility"]="Public"
     db.save(api)
@@ -93,6 +63,7 @@ def apis_publish(id):
 
 @api_bp.route('/apis/<id>/unpublish', methods=["POST"])
 def apis_unpublish(id):
+    # TODO security
     api = db.get(id)
     api["visibility"]="Private"
     db.save(api)
@@ -101,6 +72,7 @@ def apis_unpublish(id):
 
 @api_bp.route('/apis/<id>/usage')
 def apis_show_usage(id):
+    # TODO security
     return render_template('api_usage.html')
 
 
---
+++ portal/app/services/api_service.py
@@ -0,0 +1,50 @@
+import re
+
+def parse_api_object(request):
+    errors = {}
+
+    # Extract the fixed attributes
+    name = request.form.get("name", "")
+    defaultFunctionName = request.form.get("defaultFunctionName", "")
+    method = request.form.get("method", "")
+    url = request.form.get("url", "")
+    shortDescription = request.form.get("shortDescription", "")
+
+    # Validation checks
+    if not name:
+        errors['name'] = 'Name cannot be empty.'
+    # TODO: Check for name uniqueness in the database or data store
+    if not shortDescription:
+        errors['shortDescription'] = 'Short description cannot be empty.'
+    if not url:
+        errors['url'] = 'URL cannot be empty.'
+    elif not re.match(r'^https://', url):
+        errors['url'] = 'URL must start with https://'
+
+    # Initialize an empty list to store the parameters
+    params = []
+
+    # Iteratively process each pair of 'type' and 'input'
+    i = 0
+    while True:
+        param_type_key = f'param{i}-type'
+        param_input_key = f'param{i}-input'
+
+        # Check if both keys exist in the form data
+        if param_type_key in request.form and param_input_key in request.form:
+            params.append([request.form[param_type_key], request.form[param_input_key]])
+            i += 1
+        else:
+            break
+
+    api_object = {
+        "name": name,
+        "defaultFunctionName": defaultFunctionName,
+        "method": method,
+        "url": url,
+        "shortDescription": shortDescription,
+        "type": "API",
+        "params": params
+    }
+
+    return errors, api_object
