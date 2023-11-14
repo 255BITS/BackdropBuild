b255fb4946e6516a62457fe48ee6f3e798a1222e
Mikkel Garcia <mikkel@255bits.com>
2023-11-14 10:48:52 -0700

    POST /api/create

--- prototype.py
+++ prototype.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, redirect, request, jsonify, url_for
 from auth_service import validate_password, create_user, UserExistsError
 import os
+import re
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET", 'development')
@@ -56,6 +57,65 @@ def apis_my():
 @app.route('/apis/new')
 def apis_new():
     return render_template('api_new.html')
+
+@app.route('/apis/create', methods=["POST"])
+def apis_create():
+    # Check if request content type is form data
+    if request.content_type != 'application/x-www-form-urlencoded':
+        return jsonify({"error": "Content type must be application/x-www-form-urlencoded"}), 400
+
+    errors = {}
+
+    # Extract the fixed attributes
+    name = request.form.get("name", "")
+    defaultFunctionName = request.form.get("defaultFunctionName", "")
+    method = request.form.get("method", "")
+    url = request.form.get("url", "")
+    shortDescription = request.form.get("shortDescription", "")
+
+    # Validation checks
+    if not name:
+        errors['name'] = 'Name cannot be empty.'
+    # TODO: Check for name uniqueness in the database or data store
+    if not shortDescription:
+        errors['shortDescription'] = 'Short description cannot be empty.'
+    if not url:
+        errors['url'] = 'URL cannot be empty.'
+    elif not re.match(r'^https://', url):
+        errors['url'] = 'URL must start with https://'
+
+    # Initialize an empty list to store the parameters
+    params = []
+
+    # Iteratively process each pair of 'type' and 'input'
+    i = 0
+    while True:
+        param_type_key = f'param{i}-type'
+        param_input_key = f'param{i}-input'
+
+        # Check if both keys exist in the form data
+        if param_type_key in request.form and param_input_key in request.form:
+            params.append([request.form[param_type_key], request.form[param_input_key]])
+            i += 1
+        else:
+            break
+
+    api_object = {
+        "name": name,
+        "defaultFunctionName": defaultFunctionName,
+        "method": method,
+        "url": url,
+        "shortDescription": shortDescription,
+        "type": "API",
+        "params": params
+    }
+
+    # If there are errors, render the api_new template with errors
+    if errors:
+        return render_template('api_new.html', errors=errors, name=name, defaultFunctionName=defaultFunctionName, method=method, url=url, shortDescription=shortDescription, params=params)
+
+    # Add your logic here to handle the attributes and 'params' list
+    return redirect('/apis/1')
 
 @app.route('/apis/<id>')
 def apis_show(id):
