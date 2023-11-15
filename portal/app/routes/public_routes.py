c99ff76cd2cda3a20cbafe9b35e0cafacf1d1814
Martyn Garcia <martyn.garcia@gmail.com>
2023-11-14 23:41:44 -0700

    link db count

--- portal/app/routes/public_routes.py
+++ portal/app/routes/public_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
+from shared.couch import db
 
 public_bp = Blueprint('public', __name__)
 
 @public_bp.get('/')
 def home():
-    return render_template('landing.html', count_apis=123)
+    return render_template('landing.html', count_apis=db.count_apis())
