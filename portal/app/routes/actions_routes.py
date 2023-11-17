4c9175a86db336ee9e727085a252f82871c3db08
Martyn Garcia <martyn.garcia@gmail.com>
2023-11-16 23:21:17 -0700

    wip actions link_api

--- portal/app/routes/actions_routes.py
+++ portal/app/routes/actions_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, g
 from app.services.actions_service import ActionsService
 from app.services.auth_service import assert_logged_in
+from shared.couch import db
 
 actions_bp = Blueprint('actions', __name__)
 
@@ -42,24 +43,16 @@ def update(id):
     return redirect(url_for('actions.show', id=id))
 
 @actions_bp.route('/actions/<id>/link_api')
-def actions_link_api(id):
+def link_api(id):
     apis = actions_service().get_link_apis()
     return render_template('actions_link_api.html', apis=apis, id=id)
 
 @actions_bp.route('/actions/<id>/link_api/<api_id>/new')
-def actions_link_api_new(id, api_id):
-    api = {
-            "default_name": "apiCall",
-            "fields": [
-                {
-                    "api key": "credential",
-                    "bucket": "string",
-                    "message": "string"
-                 }
-            ],
-    }
-    return render_template('actions_link_api_new.html', api=api)
+def link_api_new(id, api_id):
+    api = db.get(api_id)
+    actions = actions_service().get(id)
+    return render_template('actions_link_api_new.html', api=api, actions=actions)
 
 @actions_bp.route('/api/options')
-def actions_link_api_options():
+def link_api_options():
     return render_template('actions_link_api_options.html')
