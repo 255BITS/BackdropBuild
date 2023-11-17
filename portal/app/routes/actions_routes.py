042407384a39e4bb416799bbda061a7a05c76039
Martyn Garcia <martyn.garcia@gmail.com>
2023-11-17 15:57:19 -0700

    fix action api param parsing

--- portal/app/routes/actions_routes.py
+++ portal/app/routes/actions_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, g
 from app.services.actions_service import ActionsService
 from app.services.auth_service import assert_logged_in
 from shared.couch import db
+import re
 
 actions_bp = Blueprint('actions', __name__)
 
@@ -45,11 +46,29 @@ def update(id):
 @actions_bp.get('/actions/<id>/api_link')
 def api_link(id):
     apis = actions_service().get_apis()
-    return render_template('actions_api_link.html', apis=apis, id=id)
+    actions = actions_service().get(id)
+    return render_template('actions_api_link.html', apis=apis, actions=actions)
 
 @actions_bp.post('/actions/<id>/api_link/<api_id>')
 def api_link_add(id, api_id):
-    actions_service().add_api_link(id, api_id, request.form.to_dict())
+    form_data = request.form
+    params = []
+    indexed_params = {}
+
+    # Organize the data by index
+    pattern = re.compile(r'params\[(\d+)\]\[(\w+)\]')
+    for key in form_data:
+        if key.startswith('params'):
+            index, param_key = pattern.match(key).groups()
+            if index not in indexed_params:
+                indexed_params[index] = {}
+            indexed_params[index][param_key] = form_data[key]
+
+    # Convert each indexed group into a dictionary
+    for index in indexed_params:
+        params.append(indexed_params[index])
+
+    actions_service().add_api_link(id, api_id, params)
     return redirect(url_for("actions.show", id=id))
 
 @actions_bp.route('/actions/<id>/api_link/<api_id>/new')
@@ -60,4 +79,6 @@ def api_link_new(id, api_id):
 
 @actions_bp.route('/api/options')
 def api_link_options():
-    return render_template('actions_api_link_options.html')
+    i = request.args.get("index")
+    type = request.args.get(f'params[{i}][type]')
+    return render_template('actions_api_link_options.html', type=type, i=i)
