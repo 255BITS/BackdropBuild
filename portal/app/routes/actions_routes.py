bf9e869ecfc21c4cabc87c13744d7a1df18b62a5
Martyn Garcia <martyn.garcia@gmail.com>
2023-11-16 22:35:20 -0700

    remove action list placeholder

--- portal/app/routes/actions_routes.py
+++ portal/app/routes/actions_routes.py
@@ -13,27 +13,7 @@ def before_request():
 
 @actions_bp.route('/dashboard')
 def dashboard():
-    actions_list = [
-        {
-            "_id": "abcd",
-            "name": "Calculator",
-            "apis": 2,
-            "gpts": 4,
-            "uses": 27,
-            "last_updated": "3 hours ago",
-            "sparkline_data": "0,48 60,24 120,36 180,40 240,24 300,0"
-        },
-        {
-            "_id": "efgh",
-            "name": "PDF tools",
-            "apis": 0,
-            "gpts": 0,
-            "uses": 0,
-            "last_updated": "6 days ago",
-            "sparkline_data": "0,48 60,48 120,48 180,48 240,48 300,48"
-        },
-    ]
-    actions_list += actions_service().list() #TODO pagination, order
+    actions_list = actions_service().list() #TODO pagination, order
     return render_template('dashboard.html', actions_list=actions_list)
 
 @actions_bp.route('/actions/new')
