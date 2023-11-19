2765e32f31f83175b17243fe8e9be8d0f48ee4f9
Martyn Garcia <martyn.garcia@gmail.com>
2023-11-18 22:26:27 -0700

    initial openapi spec generation

--- portal/app/routes/public_routes.py
+++ portal/app/routes/public_routes.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template
 from shared.couch import db
+from app.services.swagger_service import generate_openapi_spec_for_actions
 
 public_bp = Blueprint('public', __name__)
 
 @public_bp.get('/')
 def home():
     return render_template('landing.html', count_apis=db.count_apis())
+
+@public_bp.get('/openapi/<actions_id>.json')
+def get_openapi_spec(actions_id):
+    return generate_openapi_spec_for_actions(actions_id)
---
+++ portal/app/services/swagger_service.py
@@ -0,0 +1,54 @@
+from shared.couch import db
+
+def generate_openapi_spec_for_actions(actions_id):
+    actions = db.get(actions_id)
+    #TODO 404
+    title = actions["name"]
+    version = "1.0" #TODO
+    description = "" #TODO is this used?
+    base_url = "https://gptactionhub.com" 
+    swagger = Swagger(title, version, description, base_url)
+    apis = db.get([link['api_id'] for link in actions["api_links"]])
+    for api, api_link in zip(apis, actions["api_links"]):
+        swagger.add_api(api, api_link)
+    return swagger.generate_spec()
+
+class Swagger:
+    def __init__(self, title, version, description, base_url):
+        self.openapi_version = '3.1.0'
+        self.title = title
+        self.version = version
+        self.description = description
+        self.base_url = base_url
+        self.paths = {}
+        self.components = {'schemas': {}}
+
+    def add_api(self, api, api_link):
+        #TODO add path for api_link
+        #TODO add i/o types
+        #TODO review spec
+        pass
+
+    def add_path(self, path, methods):
+        self.paths[path] = methods
+
+    def add_schema(self, name, schema):
+        self.components['schemas'][name] = schema
+
+    def generate_spec(self):
+        return {
+            'openapi': self.openapi_version,
+            'info': {
+                'title': self.title,
+                'version': self.version,
+                'description': self.description
+            },
+            'servers': [{
+                'url': self.base_url,
+                'description': 'The hub for GPT actions'
+            }],
+            'paths': self.paths,
+            'components': self.components
+        }
+
+
