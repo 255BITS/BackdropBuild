c372e6990c6e7efd1a6ac1425280d9e7149940eb
Martyn Garcia <martyn.garcia@gmail.com>
2023-11-20 20:54:31 -0700

    add test fixtures

--- portal/tests/unit/test_swagger_service.py
+++ portal/tests/unit/test_swagger_service.py
@@ -1,13 +1,27 @@
+import json
+import os
 from app.services.swagger_service import generate_openapi_spec_for_actions
 
+def load_fixture(filename):
+    path = os.path.dirname(__file__) + "/fixtures"
+    file = os.path.join(path, filename)
+    with open(file, 'r') as f:
+        return json.load(f)
+
 def test_generate_openapi_spec_empty():
-    result = generate_openapi_spec_for_actions({
-        "api_links": [],
-        "name": "Empty"
-    }, [])
+    actions = load_fixture("empty_actions.json")
+    apis = []
+    result = generate_openapi_spec_for_actions(actions, [])
     expected = {'openapi': '3.1.0', 'info': {'title': 'Empty', 'version': '1.0', 'description': ''}, 'servers': [{'url': 'https://gptactionhub.com', 'description': 'The hub for GPT actions'}], 'paths': {}, 'components': {'schemas': {}}}
     assert len(result["servers"]) == 1
     assert result["info"]["title"] == "Empty"
     assert len(result["paths"]) == 0
     assert len(result["components"]["schemas"]) == 0
 
+def test_generate_openapi_no_params():
+    actions = load_fixture("single_no_params_actions.json")
+    apis = [load_fixture("single_no_params_api.json")]
+    result = generate_openapi_spec_for_actions(actions, apis)
+    assert len(result["servers"]) == 1
+    assert len(result["paths"]) == 0
+    assert len(result["components"]["schemas"]) == 0
