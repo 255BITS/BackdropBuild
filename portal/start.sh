1ae7234719d7053346802f3fb852b60722c70057
Martyn Garcia <martyn.garcia@gmail.com>
2023-11-27 23:33:13 -0700

    use uvicorn

--- portal/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo """[default]
-region = us-west-2""" > /root/.aws/config
-echo """[default]
-aws_access_key_id = $AWS_ACCESS_KEY_ID
-aws_secret_access_key = $AWS_SECRET_ACCESS_KEY""" > /root/.aws/credentials
-PYTHONUNBUFFERED=1 gunicorn -w 1 main:app -b 0.0.0.0:8999
